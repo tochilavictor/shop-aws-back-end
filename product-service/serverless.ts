a76765ca82bca46947a544ea97ca47f91c668c74
ViktarTachylaTR <viktar.tachyla@thomsonreuters.com>
2022-07-27 17:39:34 +0400

    better config for env variables later

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,11 +1,12 @@
 import type { AWS } from "@serverless/typescript";
 
+import env from "./env.json";
 import getProductsList from "@functions/getProductsList";
 import getProductsById from "@functions/getProductsById";
 import createProduct from "@functions/createProduct";
 
 const serverlessConfiguration: AWS = {
-  service: "product-service-4",
+  service: "product-service",
   frameworkVersion: "3",
   plugins: ["serverless-webpack", "serverless-offline"],
   provider: {
@@ -19,6 +20,11 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      PGHOST: env.PGHOST,
+      PGPORT: env.PGPORT,
+      PGDATABASE: env.PGDATABASE,
+      PGUSER: env.PGUSER,
+      PGPASSWORD: env.PGPASSWORD,
     },
   },
   // import the function via paths
